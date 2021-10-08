210468173587ba0f6f8e7d09d7d9b4038836bbe3
babakdorani <babak.dorani@gmail.com>
2021-10-08 12:00:20 +0300

    sidebar animation done. AnimationPresence added to the App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import { Switch } from "react-router";
 import Sidebar from "./components/Sidebar/Sidebar";
 import "./scss/index.scss";
+import { AnimatePresence } from "framer-motion";
 
 function App() {
   return (
+<>
     <Switch>
       <div className="App">
         <section className="sidebar">
           <Sidebar />
         </section>
-
+    <AnimatePresence exitBeforeEnter>
         <section className="content">
           <h1>test section </h1>
         </section>
+      </AnimatePresence>
       </div>
     </Switch>
+    </>
   );
 }
 
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,24 +1,74 @@
 import React from "react";
 import SidbarLinks from "./SidbarLinks";
 import "./_sidebar.scss";
+import { motion } from "framer-motion";
 
+const wrapperVariants = {
+  initial: {
+    opacity: 0,
+    x: "-20vh",
+  },
+  animate: {
+    opacity: 1,
+    x: "0vh",
+    transition: {
+      delay: 0.5,
+      duration: 1,
+      ease: "easeInOut",
+      staggerChildren: 0.5,
+    },
+  },
+  exit: {},
+};
+const childrenVariants = {
+  initial: {
+    opacity: 0,
+    y: "-4vh",
+  },
+  animate: {
+    opacity: 1,
+    y: "0vh",
+    transition: { delay: 1.5, ease: "easeOut", duration: 0.5 },
+  },
+  exit: {},
+};
+
+const footerVariants = {
+  initial: {
+    opacity: 0,
+    y: "+4vh",
+  },
+  animate: {
+    opacity: 1,
+    y: "0vh",
+    transition: { delay: 1.5, ease: "easeOut", duration: 0.5 },
+  },
+  exit: {},
+};
 const Sidebar = () => {
   return (
-    <div className="sidebar__wrapper">
+    <motion.div
+      variants={wrapperVariants}
+      initial="initial"
+      animate="animate"
+      className="sidebar__wrapper"
+    >
       <div className="sidebar__wrapper__header">
-        <h1>mefragh</h1>
+        <motion.h1>mefragh</motion.h1>
       </div>
       <div className="sidebar__wrapper__menu">
-        <ul>
+        <motion.ul variants={childrenVariants}>
           <SidbarLinks name="home" />
           <SidbarLinks name="about" />
           <SidbarLinks name="portfolio" />
           <SidbarLinks name="services" />
           <SidbarLinks name="contact" />
-        </ul>
+        </motion.ul>
       </div>
-      <div className="sidebar__wrapper__bot">Let Me Make your thought</div>
-    </div>
+      <motion.div variants={footerVariants} className="sidebar__wrapper__bot">
+        Let Me Make your thought
+      </motion.div>
+    </motion.div>
   );
 };
 
